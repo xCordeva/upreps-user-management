2744e5ddd95168a2f8b94a8d8597742b9f510f3d
Ahmad AbdEl-Aal <ahmad.abdelaallabas@gmail.com>
2025-08-01 18:48:23 +0300

    feat: add background colors to user avatars

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -1,16 +1,35 @@
 import { v4 as uuidv4 } from "uuid";
 
+// hex code colors to use as bg
+const avatarColors = [
+  "4B2548", // purple
+  "E8D2E7", // pinkish
+  "F5C9C8", // light pink
+  "C4D1E8", // light blue
+  "DEE8E9", // light cyan
+  "E8C9D5", // soft pink
+  "E8D6C9", // beige
+  "C9E8D0", // mint
+  "D6C9E8", // lavender
+];
+
+// func to choose a random bg color for each new user
+function getRandomColor() {
+  const index = Math.floor(Math.random() * avatarColors.length);
+  return avatarColors[index];
+}
+
 export function createUser(data: {
   firstName: string;
   lastName: string;
   email: string;
   role: string;
 }) {
+  const bgColor = getRandomColor();
   return {
-    // new user object concatenating name, adding avatar using first letters of first nad last name, and adding a timestamp
     id: uuidv4(),
     name: `${data.firstName} ${data.lastName}`,
-    avatar: `https://placehold.co/40x40/D6C9E8/4B2548?text=${data.firstName[0]}${data.lastName[0]}`,
+    avatar: `https://placehold.co/40x40/${bgColor}/4B2548?text=${data.firstName[0]}${data.lastName[0]}`,
     email: data.email,
     role: data.role,
     createdAt: new Date().toISOString(),
